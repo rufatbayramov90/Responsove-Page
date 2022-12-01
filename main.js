36ad274db8b908ac520d22fd13c20fd69a5fcbf4
rufatbayramov90 <rufatbyrmv@gmail.com>
2022-12-01 22:53:33 +0400

    End navbar

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-let manuBtn = document.querySelector("#menu");
-let Navbar = document.querySelector(".nav");
+let menuBtn = document.querySelector("#menuBtn");
+let navbar = document.querySelector(".nav");
 let menu = document.querySelector("#menu");
+
+menuBtn.addEventListener("click", function(){
+    if(navbar.style.right == "-200px"){
+        navbar.style.right = "0";
+        menu.src = "images/closeicon.png";
+    }else{
+        navbar.style.right = "-200px";
+        menu.src = "images/hmicon.png";
+    }
+})
